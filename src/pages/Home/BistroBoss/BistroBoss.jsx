89f773bff5d7209b8771206eb15d95482031ee46
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-05-23 02:25:18 +0600

    bistro boss section done

---
+++ src/pages/Home/BistroBoss/BistroBoss.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./BistroBoss.css";
+
+const BistroBoss = () => {
+  return (
+    <div className="bg flex justify-center items-center my-10">
+      <div className="w-8/12 text-center space-y-2 bg-white px-10 py-10">
+        <h1 className="font-bold text-center text-2xl">Bistro Boss</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, libero accusamus laborum deserunt ratione dolor
+          officiis praesentium! Deserunt magni aperiam dolor eius dolore at,
+          nihil iusto ducimus incidunt quibusdam nemo.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BistroBoss;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Catagory from '../Catagory/Catagory';
+import BistroBoss from '../BistroBoss/BistroBoss';
 
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <Catagory/>
+      <BistroBoss/>
     </div>
   );
 };
